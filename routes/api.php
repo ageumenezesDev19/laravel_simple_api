f73a8eda8e5f85665376754b1fab629f4722bea6
João Ribeiro <jribeiro@gmail.com>
2024-08-27 16:25:59 +0100

    008

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,4 @@ Route::get('/status', [ApiController::class, 'status']);
 Route::get('/clients', [ApiController::class, 'clients']);
 Route::get('/client-by-id/{id}', [ApiController::class, 'clientById']);
 Route::post('/client', [ApiController::class, 'client']);
+Route::post('/add-client', [ApiController::class, 'addClient']);
