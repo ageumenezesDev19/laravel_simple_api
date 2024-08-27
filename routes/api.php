b14aee2596bd23ad219d0693101c65eed2978154
João Ribeiro <jribeiro@gmail.com>
2024-08-27 15:52:52 +0100

    004

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ApiController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/status', function(){
-    return "Status OK";
-});
+Route::get('/status', [ApiController::class, 'status']);
+Route::get('/clients', [ApiController::class, 'clients']);
